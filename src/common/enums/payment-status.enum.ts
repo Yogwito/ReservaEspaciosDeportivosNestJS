ef6400140536eba29a5b22451d2e8c1454f1d394
valerieMCas <valeriemorenocastaneda@gmail.com>
2026-04-12 07:16:18 -0500

    feut(common): se crea el paquete common y sus componentes

---
+++ src/common/enums/payment-status.enum.ts
@@ -0,0 +1,7 @@
+export enum PaymentStatus {
+  PENDING = 'pending',
+  PROCESSING = 'processing',
+  CONFIRMED = 'confirmed',
+  FAILED = 'failed',
+  REFUNDED = 'refunded',
+}
---
+++ src/common/enums/reservation-status.enum.ts
@@ -0,0 +1,6 @@
+export enum ReservationStatus {
+  PENDING_PAYMENT = 'pending_payment',
+  ACTIVE = 'active',
+  CANCELLED = 'cancelled',
+  COMPLETED = 'completed',
+}
